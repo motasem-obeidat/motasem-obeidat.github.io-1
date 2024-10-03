86e0b12c4ea96a9ac249752cd183733374e9c82a
Motasem S Obeidat <150082417+motasem-obeidat@users.noreply.github.com>
2024-10-02 22:14:50 -0400

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name's Jane. I'm a web designer and developer based in Southampton, UK.",
-    description2: "During the day I work as a lead developer at a local agency and in the evening I  work on freelance projects and utilize the time to built my own products. I spent my leisure hours writing articles and poetry. Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas.",
+    description1: "My name's Motasem S Obeidat. I'm a PhD candidate in Computer Science and a Graduate Research Assistant in Biomedical Informatics.",
+    description2: "I am dedicated to advancing the integration of data science and healthcare. My research sits at the nexus of technology and medicine, where I specialize in natural language processing (NLP), machine learning, and data mining to drive innovations in biomedical informatics.",
     image: 2
 }
