974ddc247385a3109fd1d0bd1fe6111cd2d699fd
Motasem S Obeidat <150082417+motasem-obeidat@users.noreply.github.com>
2024-10-02 23:06:39 -0400

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -2,7 +2,7 @@ import resume from '../assets/pdf/resume.pdf'
 import pic from '../assets/pdf/PIC.jpg'
 
 export const headerData = {
-    name: 'Motasem S Obeidat',
+    name: 'Obeidat',
     title: "NLP | Machine Learning | Data Science",
     desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
     image: pic,
