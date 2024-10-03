35a96d843dc8228065d842d4e7349fb9515d461f
Motasem S Obeidat <150082417+motasem-obeidat@users.noreply.github.com>
2024-10-02 22:48:16 -0400

    Update experienceData.js

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,23 +1,16 @@
 export const experienceData = [
     {
         id: 1,
-        company: 'Globex Corporation',
-        jobtitle: 'Frontend Developer',
-        startYear: '2018',
-        endYear: '2019'
+        company: 'Yarmouk University',
+        jobtitle: 'Part-Time Instructor',
+        startYear: '2022',
+        endYear: '2023'
     },
     {
         id: 2,
-        company: 'Vehement Capital Partners',
-        jobtitle: 'Backend Developer',
-        startYear: '2019',
-        endYear: '2020'
-    },
-    {
-        id: 3,
-        company: 'Wonka Industries',
-        jobtitle: 'Marketing',
-        startYear: '2020',
+        company: 'University of Kentucky',
+        jobtitle: 'Graduate Research Assistant',
+        startYear: '2024',
         endYear: 'Present'
     },
 ]
