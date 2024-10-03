dbfd69a72981c3d7efe86e9f38503e751c567a30
Motasem S Obeidat <150082417+motasem-obeidat@users.noreply.github.com>
2024-10-02 23:29:13 -0400

    Update Main.js

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { Helmet } from 'react-helmet'
 
 import { Navbar, Footer, Landing, About, Skills, Testimonials, Blog, Education, Experience, Contacts, Projects, Services, Achievement } from '../../components'
-import { headerData } from '../../data/headerData'
+// import { headerData } from '../../data/headerData'
 
 function Main() {
     return (
         <div>
             <Helmet>
-                <title>{headerData.name} - Porfolio</title>
+                <title>Motasem S Obeidat</title>
             </Helmet>
 
             <Navbar />        
