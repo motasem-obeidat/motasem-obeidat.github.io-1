bdc0b2110abea2bc4bcc03a4c094bde10cfe1b70
Motasem S Obeidat <150082417+motasem-obeidat@users.noreply.github.com>
2024-10-03 11:31:54 -0400

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ import pic from '../assets/pdf/PIC.jpg'
 export const headerData = {
     name: 'OBEIDAT',
     title: "NLP | Machine Learning | Data Science",
-    desciption:"Welcome! Whether you're here to explore my work in NLP, Machine Learning, or Data Science, or just curious about the journey, I'm excited to share my passion with you. Let's build something great together!",
+    desciption:"Welcome! I'm thrilled to have you here. Whether you're exploring my work in NLP, Machine Learning, or Data Science, or just curious about the journey, I'm excited to share my passion and projects with you. Feel free to look around, discover what I've been working on, and don’t hesitate to reach out if you'd like to collaborate or chat. Let's build something great together!",
     image: pic,
     resumePdf: resume
 }
