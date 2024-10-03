9448506f682a87b92793bc26dda452173474cae9
Motasem S Obeidat <150082417+motasem-obeidat@users.noreply.github.com>
2024-10-02 22:43:06 -0400

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name's Motasem S Obeidat. I'm a PhD candidate in Computer Science and a Graduate Research Assistant in Biomedical Informatics.",
+    description1: "My name's Motasem S Obeidat. I'm a PhD candidate in Computer Science and a Graduate Research Assistant in Biomedical Informatics at University of Kentucky (UKY).",
     description2: "I am dedicated to advancing the integration of data science and healthcare. My research sits at the nexus of technology and medicine, where I specialize in natural language processing (NLP), machine learning, and data mining to drive innovations in biomedical informatics.",
     image: 2
 }
