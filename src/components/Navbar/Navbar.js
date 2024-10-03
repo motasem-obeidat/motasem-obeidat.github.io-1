a9972255f46e2a84a5e948a78d7554098b33821f
Motasem S Obeidat <150082417+motasem-obeidat@users.noreply.github.com>
2024-10-03 10:27:07 -0400

    Update Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,13 @@
 import React, { useContext, useState } from 'react';
 import { NavHashLink as NavLink } from 'react-router-hash-link';
 import Fade from 'react-reveal/Fade';
-import { IoMenuSharp, IoHomeSharp } from 'react-icons/io5';
-import { HiDocumentText } from 'react-icons/hi';
-import { BsFillGearFill } from 'react-icons/bs';
-import { MdPhone } from 'react-icons/md';
-import { FaUser, FaFolderOpen } from 'react-icons/fa';
-import { makeStyles } from '@material-ui/core/styles';
+import { IoMenuSharp, IoHomeSharp } from 'react-icons/io5'; // Only keep used imports
 import CloseIcon from '@material-ui/icons/Close';
+
 import './Navbar.css';
 import { headerData } from '../../data/headerData';
 import { ThemeContext } from '../../contexts/ThemeContext';
+
 
 function Navbar() {
     const { theme, setHandleDrawer } = useContext(ThemeContext);
