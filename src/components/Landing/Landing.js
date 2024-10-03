c49dea654f0d94f71a7e122ccc284d26ee60e13f
Motasem S Obeidat <150082417+motasem-obeidat@users.noreply.github.com>
2024-10-03 08:39:25 -0400

    Update Landing.js

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -2,235 +2,227 @@ import React, { useState, useContext } from 'react';
 import { Button } from '@material-ui/core';
 import { NavHashLink as NavLink } from 'react-router-hash-link';
 import { makeStyles } from '@material-ui/core/styles';
-
 import './Landing.css';
 import { ThemeContext } from '../../contexts/ThemeContext';
 import { headerData } from '../../data/headerData';
 import { socialsData } from '../../data/socialsData';
 
 import {
-    FaTwitter,
-    FaLinkedin,
-    FaGithub,
-    FaYoutube,
-    FaBlogger,
+  FaTwitter,
+  FaLinkedin,
+  FaGithub,
+  FaYoutube,
+  FaBlogger,
 } from 'react-icons/fa';
 
 function Landing() {
-    const { theme, drawerOpen } = useContext(ThemeContext);
-    const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const { theme, drawerOpen } = useContext(ThemeContext);
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
 
-    const useStyles = makeStyles((t) => ({
-        resumeBtn: {
-            color: theme.primary,
-            borderRadius: '30px',
-            textTransform: 'inherit',
-            textDecoration: 'none',
-            width: '150px',
-            fontSize: '1rem',
-            fontWeight: '500',
-            height: '50px',
-            fontFamily: 'var(--primaryFont)',
-            border: `3px solid ${theme.primary}`,
-            transition: '100ms ease-out',
-            '&:hover': {
-                backgroundColor: theme.tertiary,
-                color: theme.secondary,
-                border: `3px solid ${theme.tertiary}`,
-            },
-            [t.breakpoints.down('sm')]: {
-                width: '180px',
-            },
-        },
-        contactBtn: {
-            backgroundColor: theme.primary,
-            color: theme.secondary,
-            borderRadius: '30px',
-            textTransform: 'inherit',
-            textDecoration: 'none',
-            width: '150px',
-            height: '50px',
-            fontSize: '1rem',
-            fontWeight: '500',
-            fontFamily: 'var(--primaryFont)',
-            border: `3px solid ${theme.primary}`,
-            transition: '100ms ease-out',
-            '&:hover': {
-                backgroundColor: theme.secondary,
-                color: theme.tertiary,
-                border: `3px solid ${theme.tertiary}`,
-            },
-            [t.breakpoints.down('sm')]: {
-                display: 'none',
-            },
-        },
-    }));
+  const useStyles = makeStyles((t) => ({
+    resumeBtn: {
+      color: theme.primary,
+      borderRadius: '30px',
+      textTransform: 'inherit',
+      textDecoration: 'none',
+      width: '150px',
+      fontSize: '1rem',
+      fontWeight: '500',
+      height: '50px',
+      fontFamily: 'var(--primaryFont)',
+      border: `3px solid ${theme.primary}`,
+      transition: '100ms ease-out',
+      '&:hover': {
+        backgroundColor: theme.tertiary,
+        color: theme.secondary,
+        border: `3px solid ${theme.tertiary}`,
+      },
+      [t.breakpoints.down('sm')]: {
+        width: '100%', // Make button full width on mobile
+      },
+    },
+    contactBtn: {
+      backgroundColor: theme.primary,
+      color: theme.secondary,
+      borderRadius: '30px',
+      textTransform: 'inherit',
+      textDecoration: 'none',
+      width: '150px',
+      height: '50px',
+      fontSize: '1rem',
+      fontWeight: '500',
+      fontFamily: 'var(--primaryFont)',
+      border: `3px solid ${theme.primary}`,
+      transition: '100ms ease-out',
+      '&:hover': {
+        backgroundColor: theme.secondary,
+        color: theme.tertiary,
+        border: `3px solid ${theme.tertiary}`,
+      },
+      [t.breakpoints.down('sm')]: {
+        display: 'block',
+        width: '100%', // Make button full width on mobile
+      },
+    },
+  }));
 
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const toggleDropdown = () => {
-        setDropdownOpen(!isDropdownOpen);
-    };
+  const toggleDropdown = () => {
+    setDropdownOpen(!isDropdownOpen);
+  };
 
-    return (
-        <div className='landing'>
-            <div className='landing--container'>
-                <div
-                    className='landing--container-left'
-                    style={{ backgroundColor: theme.primary }}
-                >
-                    <div className='lcl--content'>
-                        {socialsData.linkedIn && (
-                            <a
-                                href={socialsData.linkedIn}
-                                target='_blank'
-                                rel='noreferrer'
-                            >
-                                <FaLinkedin
-                                    className='landing--social'
-                                    style={{ color: theme.secondary }}
-                                    aria-label='LinkedIn'
-                                />
-                            </a>
-                        )}
-                        {socialsData.github && (
-                            <a
-                                href={socialsData.github}
-                                target='_blank'
-                                rel='noreferrer'
-                            >
-                                <FaGithub
-                                    className='landing--social'
-                                    style={{ color: theme.secondary }}
-                                    aria-label='GitHub'
-                                />
-                            </a>
-                        )}
-                        {socialsData.twitter && (
-                            <a
-                                href={socialsData.twitter}
-                                target='_blank'
-                                rel='noreferrer'
-                            >
-                                <FaTwitter
-                                    className='landing--social'
-                                    style={{ color: theme.secondary }}
-                                    aria-label='Twitter'
-                                />
-                            </a>
-                        )}
-                        {socialsData.youtube && (
-                            <a
-                                href={socialsData.youtube}
-                                target='_blank'
-                                rel='noreferrer'
-                            >
-                                <FaYoutube
-                                    className='landing--social'
-                                    style={{ color: theme.secondary }}
-                                    aria-label='YouTube'
-                                />
-                            </a>
-                        )}
-                        {socialsData.blogger && (
-                            <a
-                                href={socialsData.blogger}
-                                target='_blank'
-                                rel='noreferrer'
-                            >
-                                <FaBlogger
-                                    className='landing--social'
-                                    style={{ color: theme.secondary }}
-                                    aria-label='Blogger'
-                                />
-                            </a>
-                        )}
-                    </div>
-                </div>
-                <img
-                    src={headerData.image}
-                    alt=''
-                    className='landing--img'
-                    style={{
-                        opacity: `${drawerOpen ? '0' : '1'}`,
-                        borderColor: theme.secondary,
-                    }}
+  return (
+    <div className='landing'>
+      <div className='landing--container'>
+        <div
+          className='landing--container-left'
+          style={{ backgroundColor: theme.primary }}
+        >
+          <div className='lcl--content'>
+            {socialsData.linkedIn && (
+              <a
+                href={socialsData.linkedIn}
+                target='_blank'
+                rel='noreferrer'
+              >
+                <FaLinkedin
+                  className='landing--social'
+                  style={{ color: theme.secondary }}
+                  aria-label='LinkedIn'
                 />
-                <div
-                    className='landing--container-right'
-                    style={{ backgroundColor: theme.secondary }}
-                >
-                    <div
-                        className='lcr--content'
-                        style={{ color: theme.tertiary }}
-                    >
-                        <h6>{headerData.title}</h6>
-                        <h1>{headerData.name}</h1>
-                        <p>{headerData.desciption}</p>
-
-                        <div className='lcr-buttonContainer'>
-                            <NavLink to='/#about' smooth={true} spy='true' duration={2000}>
-                                    <Button className={classes.contactBtn}>
-                                        About
-                                    </Button>
-                                </NavLink>
-                        {headerData.resumePdf && (
-                                    <a href={headerData.resumePdf} download='resume' target='_blank' rel='noreferrer'>
-                                        <Button className={classes.resumeBtn}>
-                                            Download CV
-                                        </Button>
-                                    </a>
-                                )}
-                            <Button onClick={toggleDropdown} className={classes.resumeBtn}>
-                                Open Menu
-                            </Button>
-
-                            <div className={`dropdown-list ${isDropdownOpen ? 'open' : ''}`}>
-                                
-
-                                <NavLink to='/#resume' smooth={true} spy='true' duration={2000}>
-                                    <Button className={classes.contactBtn}>
-                                        Education
-                                    </Button>
-                                </NavLink>
-
-                                <NavLink to='/#skills' smooth={true} spy='true' duration={2000}>
-                                    <Button className={classes.contactBtn}>
-                                        Skills
-                                    </Button>
-                                </NavLink>
-
-                                <NavLink to='/#projects' smooth={true} spy='true' duration={2000}>
-                                    <Button className={classes.contactBtn}>
-                                        Projects
-                                    </Button>
-                                </NavLink>
-
-                                <NavLink to='/#achievement' smooth={true} spy='true' duration={2000}>
-                                    <Button className={classes.contactBtn}>
-                                        News
-                                    </Button>
-                                </NavLink>
-
-                                <NavLink to='/#blog' smooth={true} spy='true' duration={2000}>
-                                    <Button className={classes.contactBtn}>
-                                        Blogs
-                                    </Button>
-                                </NavLink>
-
-                                <NavLink to='/#contacts' smooth={true} spy='true' duration={2000}>
-                                    <Button className={classes.contactBtn}>
-                                        Contact
-                                    </Button>
-                                </NavLink>
-                            </div>
-                        
-                        </div>
-                    </div>
-                </div>
-            </div>
+              </a>
+            )}
+            {socialsData.github && (
+              <a
+                href={socialsData.github}
+                target='_blank'
+                rel='noreferrer'
+              >
+                <FaGithub
+                  className='landing--social'
+                  style={{ color: theme.secondary }}
+                  aria-label='GitHub'
+                />
+              </a>
+            )}
+            {socialsData.twitter && (
+              <a
+                href={socialsData.twitter}
+                target='_blank'
+                rel='noreferrer'
+              >
+                <FaTwitter
+                  className='landing--social'
+                  style={{ color: theme.secondary }}
+                  aria-label='Twitter'
+                />
+              </a>
+            )}
+            {socialsData.youtube && (
+              <a
+                href={socialsData.youtube}
+                target='_blank'
+                rel='noreferrer'
+              >
+                <FaYoutube
+                  className='landing--social'
+                  style={{ color: theme.secondary }}
+                  aria-label='YouTube'
+                />
+              </a>
+            )}
+            {socialsData.blogger && (
+              <a
+                href={socialsData.blogger}
+                target='_blank'
+                rel='noreferrer'
+              >
+                <FaBlogger
+                  className='landing--social'
+                  style={{ color: theme.secondary }}
+                  aria-label='Blogger'
+                />
+              </a>
+            )}
+          </div>
         </div>
-    );
+        <img
+          src={headerData.image}
+          alt=''
+          className='landing--img'
+          style={{
+            opacity: `${drawerOpen ? '0' : '1'}`,
+            borderColor: theme.secondary,
+          }}
+        />
+        <div
+          className='landing--container-right'
+          style={{ backgroundColor: theme.secondary }}
+        >
+          <div
+            className='lcr--content'
+            style={{ color: theme.tertiary }}
+          >
+            <h6>{headerData.title}</h6>
+            <h1>{headerData.name}</h1>
+            <p>{headerData.desciption}</p>
+
+            <div className='lcr-buttonContainer'>
+              <NavLink to='/#about' smooth={true} spy='true' duration={2000}>
+                <Button className={classes.contactBtn}>
+                  About
+                </Button>
+              </NavLink>
+              {headerData.resumePdf && (
+                <a href={headerData.resumePdf} download='resume' target='_blank' rel='noreferrer'>
+                  <Button className={classes.resumeBtn}>
+                    Download CV
+                  </Button>
+                </a>
+              )}
+              <Button onClick={toggleDropdown} className={classes.resumeBtn}>
+                Open Menu
+              </Button>
+
+              <div className={`dropdown-list ${isDropdownOpen ? 'open' : ''}`}>
+                <NavLink to='/#resume' smooth={true} spy='true' duration={2000}>
+                  <Button className={classes.contactBtn}>
+                    Education
+                  </Button>
+                </NavLink>
+                <NavLink to='/#skills' smooth={true} spy='true' duration={2000}>
+                  <Button className={classes.contactBtn}>
+                    Skills
+                  </Button>
+                </NavLink>
+                <NavLink to='/#projects' smooth={true} spy='true' duration={2000}>
+                  <Button className={classes.contactBtn}>
+                    Projects
+                  </Button>
+                </NavLink>
+                <NavLink to='/#achievement' smooth={true} spy='true' duration={2000}>
+                  <Button className={classes.contactBtn}>
+                    News
+                  </Button>
+                </NavLink>
+                <NavLink to='/#blog' smooth={true} spy='true' duration={2000}>
+                  <Button className={classes.contactBtn}>
+                    Blogs
+                  </Button>
+                </NavLink>
+                <NavLink to='/#contacts' smooth={true} spy='true' duration={2000}>
+                  <Button className={classes.contactBtn}>
+                    Contact
+                  </Button>
+                </NavLink>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Landing;
